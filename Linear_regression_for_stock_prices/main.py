6d38c86c1152a82570b581776358f3feedf69b57
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2018-02-10 14:52:24 +0100

    linear regression stock prices: bug fixes

--- Linear_regression_for_stock_prices/main.py
+++ Linear_regression_for_stock_prices/main.py
@@ -26,15 +26,15 @@ forecast_col = 'Adj. Close'
 
 df.fillna(-9999, inplace=True)
 
-forecast_out = int(math.ceil(0.01*len(df)))
+forecast_out = int(math.ceil(0.1*len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
 X = np.array(df.drop(['label'], 1))
 X = preprocessing.scale(X)
 
-X = X[:-forecast_out]
 X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
