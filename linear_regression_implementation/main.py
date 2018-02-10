991c5f186012d44167edf9bfbf3f62e4c24f6e7e
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2018-02-10 14:45:49 +0100

    linear regression implementation: added create random data sets to check different data sets

--- linear_regression_implementation/main.py
+++ linear_regression_implementation/main.py
@@ -2,11 +2,23 @@ from statistics import mean
 import numpy as np 
 import matplotlib.pyplot as plt 
 from matplotlib import style 
+import random
 
 style.use('fivethirtyeight')
 
-xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+def create_dataset(hm, variance, step=2, correlation=False):
+    val = 1 
+    ys = []
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val+= step
+        elif correlation and correlation == 'neg':
+            val-= step    
+    xs = [i for i in range(len(ys))]
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
+
 
 def best_fit_slope_and_intercept(xs, ys):
     m = (((mean(xs) * mean(ys)) - mean(xs*ys)) / ((mean(xs)**2) - mean(xs**2)))
@@ -22,6 +34,7 @@ def coeffiecient_of_determination(ys_orig, ys_line):
     squared_error_y_mean = squared_error(ys_orig, y_mean_line)
     return 1 - (squared_error_regr / squared_error_y_mean)
 
+xs, ys = create_dataset(40, 40, 2, correlation='pos')
 
 m, b = best_fit_slope_and_intercept(xs, ys)
 
@@ -32,6 +45,7 @@ predict_x = 8
 # calculate predictions for y
 predict_y = (m*predict_x)+b
 
+# check if our regression line is good for our data
 r_squared = coeffiecient_of_determination(ys, regression_line)
 
 plt.scatter(xs, ys)
