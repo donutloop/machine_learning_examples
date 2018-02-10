e8466882b70ed191cb257bcdffcb855a16d7f2c9
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2018-02-10 11:44:24 +0100

    linear regression stock prices: save trained model into an file

--- Linear_regression_for_stock_prices/main.py
+++ Linear_regression_for_stock_prices/main.py
@@ -7,6 +7,7 @@ from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
+import pickle 
 
 style.use("ggplot")
 
@@ -37,7 +38,6 @@ X_lately = X[-forecast_out:]
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
-y = np.array(df['label'])
 
 # test size is set to 20 % of data 
 x_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
@@ -47,6 +47,13 @@ clf = LinearRegression(n_jobs=-1)
 
 # train on train data 
 clf.fit(x_train, y_train)
+
+# save trained model into a file
+with open('linearregression.pickle', 'wb') as f:
+    pickle.dump(clf, f)
+
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
 
 # score prices of test data
 accuracy = clf.score(X_test, y_test)
