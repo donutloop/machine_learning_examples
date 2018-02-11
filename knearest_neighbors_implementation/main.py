2120a36f9e6ba27bf143cdccb3dc4e7df89c967a
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2018-02-11 17:32:54 +0100

    Knearest neighbors: added clean and creating train&test set section to simple implementation

--- knearest_neighbors_implementation/main.py
+++ knearest_neighbors_implementation/main.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import warnings
 from matplotlib import style
 from collections import Counter
+import pandas as pd 
+import random
 
 style.use('fivethirtyeight')
 
@@ -26,12 +28,36 @@ def k_nearest_neighbors(data, predict, k=3):
 
     return vote_result    
 
-result = k_nearest_neighbors(dataset, new_features, k=3)
+df = pd.read_csv("breast-cancer-wisconsin.data.txt")
 
-for i in dataset:
-    for ii in dataset[i]:
-        plt.scatter(ii[0], ii[1], s=100, color=i)
+# clean data 
+df.replace('?', -9999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+full_data = df.astype(float).values.tolist()
 
-plt.scatter(new_features[0], new_features[1], color=result)
+# create train and test set 
+random.shuffle(full_data)
 
-plt.show()        
+test_size = 0.2 
+train_set = {2:[], 4:[]}
+test_set = {2:[], 4:[]}
+train_data = full_data[:-int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)):]
+
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct = 0 
+total = 0 
+
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbors(train_set, data, k=5)  
+        if group == vote:
+            correct += 1
+        total += 1
+
+print('Accuracy:', correct/total)              
