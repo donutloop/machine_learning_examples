3e4bbd0cbf5d8041f6015d1befd7a1b4266a4007
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2018-09-21 19:44:51 +0200

    RNN: preprocess data

--- tensorflow/tensorflow_keras/deep_learning_crypto/main.py
+++ tensorflow/tensorflow_keras/deep_learning_crypto/main.py
@@ -1,4 +1,8 @@
 import pandas as pd 
+from sklearn import preprocessing
+from collections import deque
+from numpy import np
+import random
 
 main_df = pd.DataFrame()
 
@@ -13,6 +17,57 @@ def classify(current, future):
         return 1
     else: 
         return 0    
+
+def preprocess_df(df):
+    df = df.drop('future', 1)
+    
+    for col in df.columns:
+        if col != "target":
+            df[col] = df[col].pct_change()
+            df.dropna(inplace=True)
+            df[col] = preprocessing.scale(df[col].values)
+    
+    df.dropna(inplace=True)        
+    sequential_data = []
+    prev_days = deque(maxlen = SEQ_LEN)
+    
+    for i in df.values:
+        prev_days.append([n for n in i[:-1]])
+        if len(prev_days) == SEQ_LEN:
+            sequential_data.append([np.array(prev_days), i[-1]])
+
+    random.shuffle(sequential_data)      
+
+    buys = []
+    sells = []
+
+    for seq, target in sequential_data:
+        if target == 0:
+            sells.append([seq, target])  
+        elif target == 1:
+            buys.append([seq, target])
+
+    random.shuffle(buys)
+    random.shuffle(sells)
+
+    lower = min(len(buys), len(sells))
+
+    buys = buys[:lower]
+    sells = buys[:lower]
+
+    sequential_data = buys+sells
+
+    random.shuffle(sequential_data)
+
+    X = []
+    Y = []
+
+    for seq, target in sequential_data:
+        X.append(seq)
+        Y.append(target)
+
+    return np.array(X), Y 
+
 
 for ratio in ratios:
     dataset = f"crypto_data/{ratio}.csv"
@@ -31,3 +86,12 @@ for ratio in ratios:
 main_df['future'] = main_df[f"{RATIO_TO_PREDICT}_close"].shift(-FUTURE_PERIOD_PREDICT)    
 
 main_df['target'] = list(map(classify, main_df[f"{RATIO_TO_PREDICT}_close"], main_df['future']))
+
+times = sorted(main_df.index.values)
+last_5pct = times[-int(0.05*len(times))]
+
+validation_main_df = main_df[(main_df.index >= last_5pct)]
+main_df = main_df[(main_df.index < last_5pct)]
+
+train_x, train_y = preprocess_df(main_df)
+validation_x, validation_y = preprocess_df(validation_main_df)
